603751ae9b849265be408dde672ce5e7981054c0
Erika Vacacela <erikavacacelaeb@gmail.com>
2022-01-26 22:01:56 -0500

    Added red-green-refactor exercise

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # tdd-ruby-start
 
+## Run local
+Install dependencies:
+
+`bundle install`
+
+This command will look your Gemfile and install the relevan Gems on the indicated versions.
+
+## Exercises
+
+* `spec.rb`. 
+Run the tests: `bundle exec rspec exercises/spec.rb`
+
 
 ### Resources
 
---
+++ exercises/spec.rb
@@ -0,0 +1,96 @@
+# Use TDD principles to build out name functionality for a Person.
+# Here are the requirements:
+# - Add a method to return the full name as a string. A full name includes
+#   first, middle, and last name. If the middle name is missing, there shouldn't
+#   have extra spaces.
+# - Add a method to return a full name with a middle initial. If the middle name
+#   is missing, there shouldn't be extra spaces or a period.
+# - Add a method to return all initials. If the middle name is missing, the
+#   initials should only have two characters.
+#
+# We've already sketched out the spec descriptions for the #full_name. Try
+# building the specs for that method, watch them fail, then write the code to
+# make them pass. Then move on to the other two methods, but this time you'll
+# create the descriptions to match the requirements above.
+
+class Person
+    def initialize(first_name:, middle_name: nil, last_name:)
+      @first_name = first_name
+      @middle_name = middle_name
+      @last_name = last_name
+    end
+
+    def full_name
+      remove_extra_spaces("#{@first_name} #{@middle_name} #{@last_name}")
+    end
+
+    def full_name_with_middle_initial
+      remove_extra_spaces("#{@first_name} #{@middle_name[0].concat('.') if @middle_name} #{@last_name}")
+    end
+
+    def initialS
+      "#{@first_name[0]}#{@middle_name[0] if @middle_name}#{@last_name[0]}"
+    end
+    
+    def remove_extra_spaces(custom_string)
+      custom_string.sub('  ', ' ')
+    end
+  end
+  
+  RSpec.describe Person do
+    describe "#full_name" do
+      it "concatenates first name, middle name, and last name with spaces" do
+        expected_full_name = 'Erika Estefania Vacacela'
+
+        actual_person = Person.new(:first_name => 'Erika', :middle_name => 'Estefania', :last_name => 'Vacacela')
+
+        expect(actual_person.full_name()).to eq(expected_full_name)
+      end
+
+      it "does not add extra spaces if middle name is missing" do
+        expected_full_name = 'Eduardo Cachavilano'
+
+        actual_person = Person.new(:first_name => 'Eduardo', :last_name => 'Cachavilano')
+
+        expect(actual_person.full_name()).to eq(expected_full_name)
+      end
+    end
+  
+    describe "#full_name_with_middle_initial" do
+      it "returns a full name with a middle initial with spaces" do
+        expected_full_name = 'Valentin W. Oliva'
+
+        actual_person = Person.new(:first_name => 'Valentin', :middle_name => 'Wos', :last_name => 'Oliva')
+
+        expect(actual_person.full_name_with_middle_initial()).to eq(expected_full_name)
+      end 
+
+      it "does not add extra spaces if middle name is missing" do
+        expected_full_name = 'Valentin Oliva'
+
+        actual_person = Person.new(:first_name => 'Valentin', :last_name => 'Oliva')
+
+        expect(actual_person.full_name_with_middle_initial()).to eq(expected_full_name)
+      end 
+
+    end
+
+  
+    describe "#initials" do
+      it "concatenates the initials of first name, middle name, and last name" do
+        expected_full_name = 'EEV'
+
+        actual_person = Person.new(:first_name => 'Erika', :middle_name => 'Estefania', :last_name => 'Vacacela')
+
+        expect(actual_person.initialS()).to eq(expected_full_name)
+      end
+
+      it "shows two characters, if middle name is missing" do
+        expected_full_name = 'EV'
+
+        actual_person = Person.new(:first_name => 'Erika', :last_name => 'Vacacela')
+
+        expect(actual_person.initialS()).to eq(expected_full_name)
+      end      
+    end
+  end
